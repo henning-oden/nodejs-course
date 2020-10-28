af6542ed6d0529431a77e306bc5240f5c14d5651
Henning Odén <henning.oden@outlook.com>
2020-10-28 11:31:52 +0100

    Fix Issue #1

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const sequelize = require('./util/database');
 const Product = require('./models/product');
 const User = require('./models/user');
 const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
 
 app.use((req, res, next) => {
     User.findByPk(1)
--- models/cart.js
+++ models/cart.js
@@ -1,5 +1,4 @@
 const Sequelize = require('sequelize');
-const Sequelize = require('sequelize');
 const sequelize = require('../util/database');
 
 const Cart = sequelize.define('cart', {
