8c0ea46f0f669883fbab3dae2d71e488d5f06044
henning-oden <73550439+henning-oden@users.noreply.github.com>
2020-10-28 11:02:38 +0100

    Lecture 166
    Updating to Lecture 166: Creating & Fetching a Cart

--- util/database.js
+++ util/database.js
@@ -1,9 +1,8 @@
-const mysql = require('mysql2');
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('node-complete', 'root', 'Dr4G0n', {
-    dialect: 'mysql',
-    host: 'localhost'
-    });
+const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
+  dialect: 'mysql',
+  host: 'localhost'
+});
 
 module.exports = sequelize;
