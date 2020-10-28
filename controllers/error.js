cff48840c63f0ad0dde6a6fefdcc8eb0f6e65887
henning-oden <73550439+henning-oden@users.noreply.github.com>
2020-10-28 17:59:40 +0100

    172 again
    The last commit failed because not all changes were actually reflected

--- controllers/error.js
+++ controllers/error.js
@@ -1,5 +1,3 @@
-exports.notFound = (req, res, next) => {
-    res.status(404).render('404', { 
-      pageTitle: 'Page Not Found',
-      path: '/404'
-     })};
+exports.get404 = (req, res, next) => {
+  res.status(404).render('404', { pageTitle: 'Page Not Found', path: '/404' });
+};
