4a82e1a10c4c1a24cf444a698d34647e014c1be2
Henning Odén <henning.oden@outlook.com>
2020-10-30 17:44:05 +0100

    minor code style fixes

--- models/product.js
+++ models/product.js
@@ -1,10 +1,10 @@
-const mongodb = require('mongodb')
+const mongodb = require('mongodb');
 const getDb = require('../util/database').getDb;
 
 class Product {
     constructor(title, price, description, imageUrl) {
-        this.title = title,
-            this.price = price,
+        this.title = title;
+            this.price = price;
             this.description = description;
         this.imageUrl = imageUrl;
     }
