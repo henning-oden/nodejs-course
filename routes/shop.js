00eb885a0141af3ceadc00e27b4463c17ae840a7
Henning Odén <henning.oden@outlook.com>
2020-10-30 17:09:28 +0100

    186. Error

--- routes/shop.js
+++ routes/shop.js
@@ -13,7 +13,7 @@ router.get('/',shopController.getIndex);
 
 router.get('/products',shopController.getProducts);
 
-// router.get('/products/:prodId', shopController.getProduct);
+router.get('/products/:prodId', shopController.getProduct);
 
 // router.get('/cart', shopController.getCart);
 // router.post('/cart', shopController.postCart);
