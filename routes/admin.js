8bf2c3bd75a7c80372b1279cceedf584efdcddd2
Henning Odén <henning.oden@outlook.com>
2020-11-03 11:44:03 +0100

    187. Edit Delete Buttons Work Again

--- routes/admin.js
+++ routes/admin.js
@@ -20,6 +20,6 @@ router.post('/add-product', adminController.postAddProduct);
 
 // router.post('/delete-product', adminController.postDeleteProduct);
 
-// router.get('/products', adminController.getProducts);
+router.get('/products', adminController.getProducts);
 
 module.exports = router;
