9d45677004b6ab0695356fae1e9731c6359d48c8
Henning Odén <henning.oden@outlook.com>
2020-11-03 12:16:38 +0100

    189. Making Update Product Work

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,5 +1,8 @@
 const e = require('express');
+const mongodb = require('mongodb');
 const Product = require('../models/product');
+
+const ObjectId = mongodb.ObjectId;
 
 exports.getAddProduct = (req, res, next) => {
     res.render('admin/edit-product', {
@@ -30,49 +33,48 @@ exports.postAddProduct = (req, res, next) => {
         });
 };
 
-// exports.getEditProduct = (req, res, next) => {
-//     const editMode = req.query.edit;
-//     if (!editMode) {
-//         res.redirect('/');
-//     }
-//     const prodId = req.params.productId;
-//     req.user.getProducts({ where: { id: prodId } })
-//         //Product.findByPk(prodId)
-//         .then(products => {
-//             const product = products[0];
-//             if (!product) {
-//                 return res.redirect('/');
-//             }
-//             res.render('admin/edit-product', {
-//                 product: product,
-//                 pageTitle: 'Edit Product',
-//                 path: '/admin/edit-product',
-//                 editing: editMode
-//             });
-//         })
-//         .catch(err => console.log(err));
-// };
+exports.getEditProduct = (req, res, next) => {
+    const editMode = req.query.edit;
+    if (!editMode) {
+        res.redirect('/');
+    }
+    const prodId = req.params.productId;
+    Product.findById(prodId)
+        //Product.findByPk(prodId)
+        .then(product => {
+            if (!product) {
+                return res.redirect('/');
+            }
+            res.render('admin/edit-product', {
+                product: product,
+                pageTitle: 'Edit Product',
+                path: '/admin/edit-product',
+                editing: editMode
+            });
+        })
+        .catch(err => console.log(err));
+};
 
-// exports.postEditProduct = (req, res, next) => {
-//     const prodId = req.body.productId;
-//     const updatedTitle = req.body.title;
-//     const updatedPrice = req.body.price;
-//     const updatedImageUrl = req.body.imageUrl;
-//     const updatedDescription = req.body.description;
-//     Product.findByPk(prodId)
-//         .then(product => {
-//             product.title = updatedTitle;
-//             product.price = updatedPrice;
-//             product.description = updatedDescription;
-//             product.imageUrl = updatedImageUrl;
-//             return product.save();
-//         })
-//         .then(result => {
-//             console.log('Updated product.');
-//             res.redirect('/admin/products');
-//         })
-//         .catch(err => console.log(err));
-// };
+exports.postEditProduct = (req, res, next) => {
+    const prodId = req.body.productId;
+    const updatedTitle = req.body.title;
+    const updatedPrice = req.body.price;
+    const updatedImageUrl = req.body.imageUrl;
+    const updatedDescription = req.body.description;
+    const product = new Product(
+        updatedTitle,
+        updatedPrice,
+        updatedDescription,
+        updatedImageUrl,
+        new ObjectId(prodId)
+    );
+    product.save()
+        .then(result => {
+    console.log('Updated product.');
+    res.redirect('/admin/products');
+})
+    .catch(err => console.log(err));
+};
 
 exports.getProducts = (req, res, next) => {
     Product.fetchAll()
--- routes/admin.js
+++ routes/admin.js
@@ -12,9 +12,9 @@ router.get('/add-product', adminController.getAddProduct);
 // /admin/add-product => POST
 router.post('/add-product', adminController.postAddProduct);
 
-// router.get('/edit-product/:productId', adminController.getEditProduct);
+router.get('/edit-product/:productId', adminController.getEditProduct);
 
-// router.post('/edit-product', adminController.postEditProduct);
+router.post('/edit-product', adminController.postEditProduct);
 
 // router.get('/delete-product/:productId', adminController.getDeleteProduct);
 
