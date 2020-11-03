626af70879341ca90b200f5cc092eeb3a305f1a2
Henning Odén <henning.oden@outlook.com>
2020-11-03 12:22:10 +0100

    190. One Note About Updating Products

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,8 +1,5 @@
 const e = require('express');
-const mongodb = require('mongodb');
 const Product = require('../models/product');
-
-const ObjectId = mongodb.ObjectId;
 
 exports.getAddProduct = (req, res, next) => {
     res.render('admin/edit-product', {
@@ -66,7 +63,7 @@ exports.postEditProduct = (req, res, next) => {
         updatedPrice,
         updatedDescription,
         updatedImageUrl,
-        new ObjectId(prodId)
+        prodId
     );
     product.save()
         .then(result => {
