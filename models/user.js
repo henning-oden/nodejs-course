00c844b9b25ee485a5ef0aa21d9b14762701aeea
Henning Odén <henning.oden@outlook.com>
2020-11-04 16:14:29 +0100

    203. Getting Orders

--- models/user.js
+++ models/user.js
@@ -77,9 +77,17 @@ class User {
 
     addOrder() {
         const db = getDb();
-        return db // This return statement was initially dropped, causing TypeError in shop.js:120:5
-            .collection('orders')
-            .insertOne(this.cart)
+        return this.getCart()
+            .then(products => {
+                const order = {
+                    items: products,
+                    user: {
+                        _id: new mongodb.ObjectId(this._id),
+                        name: this.name
+                    }
+                };
+                return db.collection('orders').insertOne(order);
+            })
             .then(result => {
                 this.cart = { items: [] };
                 return db
@@ -89,6 +97,13 @@ class User {
                         { $set: { cart: { items: [] } } }
                     );
             });
+    }
+
+    getOrders() {
+        const db = getDb();
+        return db.collection('orders')
+            .find({ 'user._id': new mongodb.ObjectId(this._id) })
+            .toArray();
     }
 
     static findById(userId) {
