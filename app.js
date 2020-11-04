5ddddce6808c51a465075d95a092a87f4a9da832
Henning Odén <henning.oden@outlook.com>
2020-11-04 17:43:02 +0100

    212. Saving Data Through Mongoose

--- app.js
+++ app.js
@@ -18,21 +18,21 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-  User.findById('5fa17c61c774ecff1ab7bf74')
-    .then(user => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
-      next();
-    })
-    .catch(err => console.log(err));
-});
+// app.use((req, res, next) => {
+//   User.findById('5fa17c61c774ecff1ab7bf74')
+//     .then(user => {
+//       req.user = new User(user.name, user.email, user.cart, user._id);
+//       next();
+//     })
+//     .catch(err => console.log(err));
+// });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoose.connect('mongodb+srv://henning-user:Dr4G0nW1n@cluster0.o3hei.mongodb.net/test?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://henning-user:Dr4G0nW1n@cluster0.o3hei.mongodb.net/shop?retryWrites=true&w=majority')
 .then(result => {
   app.listen(3000);
 }).catch(err => {
