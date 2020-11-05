d058866d9cbf49966799b4b1705da2aae04d6420
Henning Odén <henning.oden@outlook.com>
2020-11-05 16:39:45 +0100

    219. One Important Thing About Fetching Relations

--- controllers/admin.js
+++ controllers/admin.js
@@ -81,7 +81,10 @@ exports.postEditProduct = (req, res, next) => {
 
 exports.getProducts = (req, res, next) => {
     Product.find()
+        // .select('-_id -description -imageUrl')
+        // .populate('userId', 'name')
         .then(products => {
+            console.log(products);
             res.render('admin/products', {
                 prods: products,
                 pageTitle: 'Admin Products',
