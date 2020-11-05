517c6994bfb672c80443090152a2eaf37c94b738
Henning Odén <henning.oden@outlook.com>
2020-11-05 15:58:19 +0100

    216. Deleting Products

--- controllers/admin.js
+++ controllers/admin.js
@@ -94,7 +94,7 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
     const prodId = req.body.productId;
-    Product.deleteById(prodId)
+    Product.findByIdAndRemove(prodId)
         .then(() => {
             console.log('Destroyed product.');
             res.redirect('/admin/products');
