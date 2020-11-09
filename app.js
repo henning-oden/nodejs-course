d1c29dff8410b7d2ae76e7753d8a136ac7855efc
Henning Odén <henning.oden@outlook.com>
2020-11-09 15:08:12 +0100

    240. Using MongoDB to Store Sessions

--- app.js
+++ app.js
@@ -3,12 +3,18 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const session = require('express-session');
-const db = require('./util/database');
+const mongoose = require('mongoose');
+const MongoDBStore = require('connect-mongodb-session')(session);
+const MONGODB_URI = require('./util/database');
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
 const app = express();
+const store = new MongoDBStore({
+    uri: MONGODB_URI,
+    collection: 'sessions'
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -19,15 +25,22 @@ const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({secret: 'my secret', resave: false, saveUninitialized: false }));
+app.use(
+    session({
+        secret: 'my secret',
+        resave: false,
+        saveUninitialized: false,
+        store: store
+    })
+);
 
 app.use((req, res, next) => {
-  User.findById('5fa414ec5143906158509051')
-    .then(user => {
-      req.user = user;
-      next();
-    })
-    .catch(err => console.log(err));
+    User.findById('5fa414ec5143906158509051')
+        .then(user => {
+            req.user = user;
+            next();
+        })
+        .catch(err => console.log(err));
 });
 
 app.use('/admin', adminRoutes);
@@ -36,10 +49,10 @@ app.use(authRoutes);
 
 app.use(errorController.get404);
 
-db.connect()
+mongoose.connect(MONGODB_URI)
     .then(result => {
         User.findOne().then(user => {
-            if (!user) { 
+            if (!user) {
                 const user = new User({
                     name: 'Max',
                     email: 'max@test.com',
