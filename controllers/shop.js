d4e330ec252098d0549b198ed6860d2363284e0c
Henning Odén <henning.oden@outlook.com>
2020-11-10 17:15:01 +0100

    261. Adding CSRF Protection

--- controllers/shop.js
+++ controllers/shop.js
@@ -38,8 +38,7 @@ exports.getIndex = (req, res, next) => {
         prods: products,
         pageTitle: 'Shop',
         path: '/',
-        isAuthenticated: req.session.isLoggedIn,
-        csrfToken: req.csrfToken()
+        isAuthenticated: req.session.isLoggedIn
       });
     })
     .catch(err => {
