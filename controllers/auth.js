f0bafaf5a20a59cb30d483288347b5579ab0386c
Henning Odén <henning.oden@outlook.com>
2020-11-10 15:58:05 +0100

    255. Encrypting Passwords With Code Improvement

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcryptjs');
+
 const User = require('../models/user');
 
 exports.getLogin = (req, res, next) => {
@@ -38,15 +40,18 @@ exports.postSignup = (req, res, next) => {
             if(userDoc) {
                 return res.redirect('/signup');
             }
-            const user = new User({
-                email: email,
-                password: password,
-                cart: { items: [] }
+            return bcrypt.hash(password, 12)
+            .then(hashedPassword => {
+                const user = new User({
+                    email: email,
+                    password: hashedPassword,
+                    cart: { items: [] }
+                });
+                return user.save();
+            })
+            .then(result => {
+                res.redirect('/login');
             });
-            return user.save();
-        })
-        .then(result => {
-            res.redirect('/login');
         })
         .catch(err => {
             console.log(err);
