ddfcac13f1d920045028eecbce3af4ea46041763
Henning Odén <henning.oden@outlook.com>
2020-11-10 15:29:37 +0100

    252. Our Updated Project Status

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,17 @@
 const User = require('../models/user');
 
 exports.getLogin = (req, res, next) => {
-    console.log(req.session.isLoggedIn);
     res.render('auth/login', {
         path: '/login',
         pageTitle: 'Login',
+        isAuthenticated: false
+    });
+}
+
+exports.getSignup = (req, res, next) => {
+    res.render('auth/signup', {
+        path: '/signup',
+        pageTitle: 'Signup',
         isAuthenticated: false
     });
 }
@@ -21,6 +28,8 @@ exports.postLogin = (req, res, next) => {
         })
         .catch(err => console.log(err));
 }
+
+exports.postSignup = (req, res, next) => {};
 
 exports.postLogout = (req, res, next) => {
     req.session.destroy(err => {
