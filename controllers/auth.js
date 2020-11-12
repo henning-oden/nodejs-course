8ec6a42656a05a8278ac883c292be29c7f8e58df
Henning Odén <henning.oden@outlook.com>
2020-11-12 17:54:48 +0100

    290. Setup & Basic Validation

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,6 +4,7 @@ const bcrypt = require('bcryptjs');
 const nodemailer = require('nodemailer');
 const sendgridTransport = require('nodemailer-sendgrid-transport');
 const apiKey = require('../util/sendgrid');
+const { validationResult } = require('express-validator/check')
 
 const User = require('../models/user');
 
@@ -83,6 +84,17 @@ exports.postSignup = (req, res, next) => {
     const email = req.body.email;
     const password = req.body.password;
     const confirmPassword = req.body.confirmPassword;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        console.log(errors.array())
+        return res.status(422).render(
+            res.render('auth/signup', {
+                path: '/signup',
+                pageTitle: 'Signup',
+                errorMessage: errors.array()
+            })
+        );
+    }
     User.findOne({ email: email })
         .then(userDoc => {
             if (userDoc) {
