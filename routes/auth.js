e3ba51d0f08e0965e4fb772fc0de99688cd948b9
Henning Odén <henning.oden@outlook.com>
2020-11-12 18:09:00 +0100

    293. More Validators

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { check } = require('express-validator/check');
+const { check, body } = require('express-validator/check');
 const authController = require('../controllers/auth');
 
 const router = express.Router();
@@ -14,7 +14,19 @@ router.post(
     '/signup', 
     check('email')
         .isEmail()
-        .withMessage('Please enter a valid email.'), 
+        .withMessage('Please enter a valid email.')
+        .custom((value, {req}) => {
+            if (value === 'test@test.com') {
+                throw new Error('This email address is forbidden.');
+            }
+            return true;
+        }),
+    body(
+        'password',
+        'Please enter a password with only numbers and text and at least 5 characters.'
+    )
+        .isLength({ min: 5 })
+        .isAlphanumeric(),
     authController.postSignup
 );
 
