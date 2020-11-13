1304319e6a7a4eeed99ce0defaa3cf5fb703bb14
Henning Odén <henning.oden@outlook.com>
2020-11-13 13:39:52 +0100

    294. Checking for Field Equality

--- routes/auth.js
+++ routes/auth.js
@@ -27,6 +27,13 @@ router.post(
     )
         .isLength({ min: 5 })
         .isAlphanumeric(),
+    body('confirmPassword')
+        .custom((value, {req}) => {
+            if (value !== req.body.password) {
+                throw new Error('Passwords have to match!');
+            }
+            return true;
+        }),
     authController.postSignup
 );
 
