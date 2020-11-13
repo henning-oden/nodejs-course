769f43cfce02686c01685cecfb96f5dff9c34331
Henning Odén <henning.oden@outlook.com>
2020-11-13 14:02:05 +0100

    Assignment 6: Time to Practice - Validation

--- routes/auth.js
+++ routes/auth.js
@@ -10,7 +10,14 @@ router.get('/login', authController.getLogin);
 
 router.get('/signup', authController.getSignup);
 
-router.post('/login', authController.postLogin);
+router.post('/login',
+    check('email')
+        .isEmail()
+        .withMessage('Please enter a valid email.'),
+    body('password', 'Please enter a password at least 5 characters long containing only letters and numbers.')
+        .isLength({ min: 5 })
+        .isAlphanumeric(),
+    authController.postLogin);
 
 router.post(
     '/signup',
