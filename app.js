2fdceb7b8d0317b1ff72fa71a8f7e3f528f7ac14
Henning Odén <henning.oden@outlook.com>
2020-11-16 15:59:22 +0100

    312. Using the Error Handling Middleware Correctly

--- app.js
+++ app.js
@@ -38,6 +38,11 @@ app.use(
 );
 app.use(csrfProtection);
 app.use(flash());
+app.use((req, res, next) =>  {
+    res.locals.isAuthenticated = req.session.isLoggedIn;
+    res.locals.csrfToken = req.csrfToken();
+    next();
+})
 
 app.use((req, res, next) => {
     if (!req.session.user) {
@@ -52,15 +57,9 @@ app.use((req, res, next) => {
         next();
       })
       .catch(err => {
-          throw new Error(err);
+          next(new Error(err));
       });
   });
-
-app.use((req, res, next) =>  {
-    res.locals.isAuthenticated = req.session.isLoggedIn;
-    res.locals.csrfToken = req.csrfToken();
-    next();
-})
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -72,7 +71,8 @@ app.use(errorController.get404);
 
 app.use((error, req, res, next) => {
     // res.status(errpr.httpStatusCode).render(...);
-    res.redirect('/500');
+    res.status(500).render('500', { pageTitle: 'Error!', path: '/500',
+    isAuthenticated: req.session.isLoggedIn });
 })
 
 mongoose.connect(MONGODB_URI)
